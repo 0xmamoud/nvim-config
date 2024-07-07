d95c602e6de0b30caa58d841257f83e2d75a9e15
mamoud <mamoud.antacid895@passmail.com>
2024-07-07 20:04:00 +0200

    feat: added import auto organize

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -1,13 +1,39 @@
-local on_attach = require("plugins.configs.lspconfig").on_attach
-local capabilities = require("plugins.configs.lspconfig").capabilities
+local config = require("plugins.configs.lspconfig")
+local on_attach = config.on_attach
+local capabilities = config.capabilities
 
 local lspconfig = require("lspconfig")
+
+local function organize_imports()
+  local params = {
+    command = "_typescript.organizeImports",
+    arguments = {vim.api.nvim_buf_get_name(0)},
+  }
+  vim.lsp.buf.execute_command(params)
+end
 
 local servers = {"tsserver", "tailwindcss", "eslint"}
 
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
+  local opts = {
     on_attach = on_attach,
     capabilities = capabilities,
   }
+
+  if lsp == "tsserver" then
+    opts.init_options = {
+      preferences = {
+        disableSuggestions = true,
+      }
+    }
+    opts.commands = {
+      OrganizeImports = {
+        organize_imports,
+        description = "Organize Imports",
+      }
+    }
+  end
+
+  lspconfig[lsp].setup(opts)
 end
+
